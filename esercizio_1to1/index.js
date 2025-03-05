94b11ce6cb3e78371f656bb06f9c6ebc901a7621
reverberi74 <luca.calvo44@gmail.com>
2025-03-05 21:52:17 +0100

    Array

--- esercizio_1to1/index.js
+++ esercizio_1to1/index.js
@@ -2,7 +2,7 @@
 */ 
 
 function stringLong (arrayStringhe){
-    if(!Array.isArray(numbers) || numbers.length === 0){
+    if(!Array.isArray(arrayStringhe) || arrayStringhe.length === 0){
         console.log("Inserisci un array valido");
         return;
     }
